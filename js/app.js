1a88684c80b5e1cfcd1ea9d8fea8e350862ee6c3
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-13 18:46:18 -0600

    second step

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var userName = "admin";
+var userAge = 38;
+var isMember = true;
+
+if(userName == "admin" && userAge >= 35 && isMember == true){
+    console.log("welcome admin well aged");
+}else{
+    console.log("welcome stranger");
+}
